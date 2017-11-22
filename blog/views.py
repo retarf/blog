a61b1dd0853523fb7cb65759df22bff80130c453
retarf <retarf@draco.localdomain>
2017-11-22 18:27:29 +0100

    search view

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,10 @@ from django.db.models import Count
 
 from taggit.models import Tag
 
+from haystack.query import SearchQuerySet
+
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 
 # class PostListView(ListView):
@@ -87,3 +89,20 @@ def post_share(request, post_id):
         form = EmailPostForm()
 
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            cd = form.cleaned_data
+            results = SearchQuerySet().models(Post).filter(
+                content=cd['query']).load_all()
+            total_results = results.count()
+            return render(request, 'blog/post/search.html', {'form': form,
+                                                             'cd': cd,
+                                                             'results': results,
+                                                             'total_results': total_results})
+        else:
+            return render(request, 'blog/post/search.html', {'form': form})
--- mysite/settings.py
+++ mysite/settings.py
@@ -51,11 +51,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'haystack',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
